e9056d8a2c10321d2d757dca62d67d75a168d0e0
Matthew-IR <67317204+Matthew-IR@users.noreply.github.com>
2025-09-25 23:12:07 +0100

    Add descriptors for Server class functions and private variables

--- include/network.hpp
+++ include/network.hpp
@@ -6,20 +6,38 @@
 
 class Server {
     private:
-        int server_fd, new_socket;
-        struct sockaddr_in addr;
+        int server_fd, new_socket; ///< File descriptor for the server and client sockets.
+        struct sockaddr_in addr; ///< Address structure for the server.
         int ip_len;
-        const int PORT;
+        const int PORT; ///< The port number the server is listening on.
     
     public:
+        /** 
+         * @brief Constructs a new Server object.
+         * @param port The port number on which the server listens
+        */
         Server(int port);
         
+        /** 
+         * @brief Listens for incoming network connections.
+        */
         void network_listen();
 
+        /** 
+         * @brief Sends data to the connected client.
+         * @param data The string of data that is being sent over the socket.
+        */
         void send_data(const std::string& data);
 
+        /** 
+         * @brief Receives data from the connected client.
+         * @return The data that has received as a string.
+        */
         std::string receive_data();
 
+        /** 
+         * @brief Desstructor for the Server class.
+        */
         ~Server();
 
 };
