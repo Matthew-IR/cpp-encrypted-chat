3cf7d02ec7e945aca882b0030dd13ad01783c4d5
Matthew-IR <67317204+Matthew-IR@users.noreply.github.com>
2025-09-25 22:03:02 +0100

    Create a server and client class

--- include/network.hpp
+++ include/network.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <sys/socket.h>
 
 class Network {
     public:
@@ -17,4 +18,38 @@ class Network {
     
     private:
         int client_socket;
+};
+
+
+class Server {
+    private:
+        int server_fd, new_socket;
+        struct sockaddr_in addr;
+        int ip_len;
+        const int PORT;
+    
+    public:
+        Server(int port);
+        
+        void network_listen();
+
+        void send_data(const std::string& data);
+
+        std::string receive_data();
+
+        ~Server();
+
+};
+
+
+class Client {
+    private:
+        int sock;
+        struct sockaddr_in server_addr;
+        const int PORT;
+
+    public:
+        Client(const std::string& server_ip, int port);
+
+        ~Client();
 };
